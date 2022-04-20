f4dbe548e05ea683fe605e9274937e8d3ea44c67
Collins <mutaic84@gmail.com>
2022-04-20 12:32:31 +0300

    display each character details on page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const charactercontainer = document.getElementById("character-bar");
+    const characterInfoContainer = document.querySelector("div.characterInfo")
 
     fetch("http://localhost:3000/characters")
     .then(response => response.json())
     .then(data => {
         data.forEach(element => {
-            // console.log(element)
+            console.log(element)
             displayCharacter(element)
+            displayCharacterDetails(1)
         });
     })
 
@@ -23,8 +25,25 @@ document.addEventListener("DOMContentLoaded", () => {
     function displayCharacterDetails(id) {
         fetch(`http://localhost:3000/characters/${id}`)
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
+        .then(character => {
+            const itemContainer = `
+            <div id="detailed-info">
+                <p id="name">${character.name}</p>
+                <img
+                id="image"
+                src="${character.image}"
+                alt="${character.name}"
+                /><!-- display character image here -->
+                <h4>Total Votes: <span id="vote-count">${character.votes}</span></h4>
+                <form id="votes-form">
+                <input type="text" placeholder="Enter Votes" id="votes" name="votes" />
+                <input type="submit" value="Add Votes" />
+                </form>
+                <button id="reset-btn">Reset Votes</button>
+            </div>
+            `
+            characterInfoContainer.innerHTML = ''
+            characterInfoContainer.innerHTML = itemContainer
         })
     }
 })
