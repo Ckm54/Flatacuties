0c294e03f5bffe14bc9cfe59e62e9fd8af1611ca
Collins <mutaic84@gmail.com>
2022-04-20 12:16:10 +0300

    display character names in span

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const charactercontainer = document.getElementById("character-bar");
+
+    fetch("http://localhost:3000/characters")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            console.log(element)
+            displayCharacter(element)
+        });
+    })
+
+    function displayCharacter(character) {
+        const spanContainer = document.createElement("span")
+        spanContainer.innerText = character.name
+        charactercontainer.append(spanContainer)
+    }
+})
